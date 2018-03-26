a4d7fd55a5c0bf0d39ecd317fd77fdba8bc348a8
vanquang2 <37770772+vanquang2@users.noreply.github.com>
2018-03-26 08:06:38 +0700

    Update tongcacso.java

--- tongcacso.java
+++ tongcacso.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class tongcacso {
 	public static void main(String[] args) {
-	    int n, soDu, tong = 0;
+	    int n, a, tong = 0;
 	    Scanner scanner = new Scanner(System.in);
 	         
 	    System.out.println("Nhap so: ");
 	    n = scanner.nextInt();
 	         
 	    while (n > 0) {
-	        soDu = n % 99;
-	        n = n / 99;
-	        tong += soDu;
+	        a = n % 10;
+	        n = n / 10;
+	        tong += a;
 	    }
 	         
 	    System.out.println("Tong cac chu so = " + tong);
